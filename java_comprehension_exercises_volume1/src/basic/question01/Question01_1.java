2bad44810090690862b4acdeffaf191b4311f0e2
milky245 <xuezhihan@toki.waseda.jp>
2026-04-16 12:31:33 +0900

    0416 am upload all exercises answer 1-7

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -9,6 +9,12 @@ public class Question01_1 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
+		int price = 120;
+		String item = "ノート";
+		System.out.printf("%d\n%s\n", price, item);
+		price = 200;
+		item = "鉛筆";
+		System.out.printf("%d\n%s\n", price, item);
 
 	}
 
